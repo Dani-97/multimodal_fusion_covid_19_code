c359256b4b3a9f808116a5af5bb192f344132293
Dani-97 <daniel.iglesias.moris@outlook.es>
2022-04-22 11:54:02 +0200

    We have changed the visualization of the dispersion of a single variable. Now, instead of a scatter visualization, it is a histogram visualization.

--- analysis/utils_analysis_particular_attrs.py
+++ analysis/utils_analysis_particular_attrs.py
@@ -18,7 +18,7 @@ class Analysis_Dispersion_1D():
 
         return indexes_list
 
-    def execute_analysis(self, input_dataframe, \
+    def execute_analysis_scatter(self, input_dataframe, \
                                            dir_to_store_analysis, attrs_list):
         var1 = np.array(input_dataframe[attrs_list[0]])
 
@@ -48,6 +48,52 @@ class Analysis_Dispersion_1D():
 
         print('++++ The dispersion study has been stored at %s'%output_filename)
 
+    def execute_analysis(self, input_dataframe, dir_to_store_analysis, \
+                                                         attrs_list, nbins=6):
+        var1 = np.array(input_dataframe[attrs_list[0]])
+
+        var1_ok_idxs = self.__remove_missing_values_idxs__(var1.tolist())
+
+        # This variable stores all the indexes without missing values.
+        ok_idxs = var1_ok_idxs
+
+        output_array = np.array(input_dataframe['output'].tolist()).reshape(1, -1)
+        class0_idxs = list(set(np.where(output_array==0)[1].tolist()).intersection(ok_idxs))
+        class1_idxs = list(set(np.where(output_array==1)[1].tolist()).intersection(ok_idxs))
+
+        fig, ax = plt.subplots()
+        patches = ax.patches
+
+        var1_class0_for_hist, _ = np.histogram(np.sort(var1[class0_idxs]), bins=nbins)
+        var1_class1_for_hist, _ = np.histogram(np.sort(var1[class1_idxs]), bins=nbins)
+
+        max_value = np.max(var1[ok_idxs])
+        min_value = np.min(var1[ok_idxs])
+        step = (max_value-min_value)/nbins
+        xlabels = []
+
+        for it in range(0, nbins):
+            min_lim = min_value+(it*step)
+            max_lim = min_value+((it+1)*step)
+            xlabels.append('%d-%d'%(min_lim, max_lim))
+
+        legend = ['Class 0', 'Class 1']
+        x_class0_values = np.array(list(range(0, len(var1_class0_for_hist)*2, 2)))
+        plt.bar(x_class0_values, var1_class0_for_hist, width=0.5)
+        x_class1_values = np.array(list(range(1, len(var1_class1_for_hist)*2, 2)))
+        plt.bar(x_class1_values, var1_class1_for_hist, width=0.5)
+        plt.xlabel(attrs_list[0])
+        plt.legend(legend)
+
+        bars_width = patches[0].get_width()
+        plt.xticks(x_class0_values+bars_width, xlabels)
+
+        output_filename = '%s/%s'%(dir_to_store_analysis, \
+                'dispersion_analysis_%s.pdf'%(attrs_list[0]))
+        plt.savefig(output_filename)
+
+        print('++++ The dispersion study has been stored at %s'%output_filename)
+
 class Analysis_Dispersion_2D():
 
     def __init__(self):
@@ -64,8 +110,8 @@ class Analysis_Dispersion_2D():
 
         return indexes_list
 
-    def execute_analysis(self, input_dataframe, \
-                                           dir_to_store_analysis, attrs_list):
+    def execute_analysis(self, input_dataframe, dir_to_store_analysis, \
+                                                        attrs_list, nbins=6):
         var1 = np.array(input_dataframe[attrs_list[0]])
         var2 = np.array(input_dataframe[attrs_list[1]])
 
